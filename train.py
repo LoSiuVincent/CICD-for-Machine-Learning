e8e39c06a5b300e11eb80a37e32f50cf8527d814
Vincent Lo <lovincent536@gmail.com>
2025-05-06 11:04:35 +0800

    fix: model result image has no content

--- train.py
+++ train.py
@@ -49,6 +49,7 @@ with open('Results/metrics.txt', 'w') as outfile:
 
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
+disp.plot()
 plt.savefig('Results/model_results.png', dpi=120)
 
 sio.dump(pipe, 'Model/drug_pipeline.skops')
